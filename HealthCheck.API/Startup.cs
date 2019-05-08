e603ea9cb77fbbfe51068410d13a94ba1f9dd646
Jp <jp.roussell@entelect.co.za>
2019-05-08 14:16:26 +0200

    Clean up dependencies in code
    Also want to see if Azure DevOps build runs automatically

--- HealthCheck.API/Startup.cs
+++ HealthCheck.API/Startup.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using HealthCheck.API.Services;
-using HealthCheck.Model;
+﻿using HealthCheck.API.Services;
 using HealthCheck.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace HealthCheck.API
 {
